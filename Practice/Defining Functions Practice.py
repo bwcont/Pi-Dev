fcbe7442314ab4be738c1591b938b208b55c84ca
E.C <35582713+bwcont@users.noreply.github.com>
2018-11-24 10:49:08 -0600

    Update Defining Functions Practice.py

--- Practice/Defining Functions Practice.py
+++ Practice/Defining Functions Practice.py
@@ -8,5 +8,12 @@ def square(x):
 def sum_of_squares(x, y):
   return square(x) + square(y)
 
+#If else number ten definitions
+def numberten(x):
+    if x > 10:
+        print ("x is greater than 10")
+    else:
+        print ("x is not greater than 10")
+
 ##Maximums excercise
 
